f3b270a001983a9d6f4708a67eaeafb2c89f5235
Matt Engdahl <matt.engdahl@gmail.com>
2016-07-05 12:40:57 +1000

    Progress

--- reproResearch.R
+++ reproResearch.R
@@ -1,3 +1,6 @@
+## Include packages
+library(dplyr)
+
 ## Download and read source data
 
 temp <- tempfile()
@@ -6,3 +9,26 @@ data <- unzip(temp, "activity.csv")
 data <- read.csv(data)
 unlink(temp)
 
+## Data processing
+data[,2] <- as.Date(data[,2], format = "%Y-%m-%d")
+
+## Histogram of the total number of steps taken each day
+steps <- aggregate(data$steps, list(data$date), FUN = "sum")
+
+hist(steps[ ,2], breaks = 10, main = "Histogram of number of steps taken per day", xlab = "Number of steps", ylab = "Number of days")
+
+## Mean number of steps taken per day
+mean(steps[ ,2], na.rm = TRUE)
+median(steps[ ,2], na.rm = TRUE)
+
+## Time series plot
+interval.steps <- aggregate(data$steps, list(data$interval), FUN = "mean", na.rm = TRUE)
+plot(interval.steps[,1], interval.steps[,2], type = "l", main = "Average steps taken by interval", xlab = "Interval (time of day)", ylab = "Number of steps taken")
+
+## Number of missing step values in data
+sum(is.na(data$steps))
+
+## Strategy; use median value for each relevant time interval as closest approximation
+interval.steps.median <- aggregate(data$steps, list(data$interval), FUN = "median", na.rm = TRUE)
+colnames(interval.steps.median) <- c("interval", "median.steps")
+dplyr::full_join(data, interval.steps.median, by = "interval")
