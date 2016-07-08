51901d9e2ddb730d2f276d34fc858e2df71e9981
Matt Engdahl <matt.engdahl@gmail.com>
2016-07-08 10:50:19 +1000

    Final updates

--- reproResearch.R
+++ reproResearch.R
@@ -25,6 +25,9 @@ median(steps[ ,2], na.rm = TRUE)
 interval.steps <- aggregate(data$steps, list(data$interval), FUN = "mean", na.rm = TRUE)
 plot(interval.steps[,1], interval.steps[,2], type = "l", main = "Average steps taken by interval", xlab = "Interval (time of day)", ylab = "Number of steps taken")
 
+## Maximum number of steps in which interval
+max(interval.steps)
+
 ## Number of missing step values in data
 sum(is.na(data$steps))
 
@@ -42,4 +45,36 @@ merged$steps[is.na(merged$steps)] <- merged$median.steps[is.na(merged$steps)]
 ### 4. Delete interval.steps.median variable
 merged <- merged[,1:ncol(merged) - 1]
 
+## Updated histogram, mean, and median
+steps <- aggregate(merged$steps, list(merged$date), FUN = "sum")
+hist(steps[ ,2], breaks = 10, main = "Histogram of number of steps taken per day", xlab = "Number of steps", ylab = "Number of days")
+mean(steps[ ,2], na.rm = TRUE)
+median(steps[ ,2], na.rm = TRUE)
 
+## Set weekdays
+merged <- cbind(merged, day.of.week = weekdays(merged$date))
+
+## Create factor with weekday/weekend
+merged$day.of.week <- as.character(merged$day.of.week)
+merged$day.of.week[merged$day.of.week == "Sunday" | merged$day.of.week == "Saturday"] <- "Weekend"
+merged$day.of.week[merged$day.of.week != "Weekend"] <- "Weekday"
+merged$day.of.week <- as.factor(merged$day.of.week)
+
+## Mean number of steps taken for each interval during weekdays
+steps.weekend <- aggregate(merged$steps[merged$day.of.week == "Weekend"], list(merged$interval[merged$day.of.week == "Weekend"]), FUN = "mean")
+steps.weekday <- aggregate(merged$steps[merged$day.of.week == "Weekday"], list(merged$interval[merged$day.of.week == "Weekday"]), FUN = "mean")
+
+## Panel plot
+par(mfrow = c(2, 1),  oma = c(0, 1, 0, 0), mar = c(4, 4, 2, 2), cex = 0.8)
+par(mai = c(0, 0.5, 0.6, 0.4))
+plot(steps.weekend[ ,1], steps.weekend[ ,2], ylim = c(0, 200), type = "l", axes = FALSE, frame.plot = TRUE,
+     xlab = "", ylab = "")
+title("Weekend", line = -2)
+Axis(side = 4, labels = TRUE)
+mtext("Comparison of weekend vs. weekday step counts", side = 3, line = 1.5, cex = 1.1)
+mtext("Mean steps taken", side = 2, line = -.5, cex = .9, outer = TRUE)
+
+par(mai = c(0.6, 0.5, 0, 0.4))
+plot(steps.weekday[ ,1], steps.weekday[ ,2], ylim = c(0, 200), type = "l", xlab = "", ylab = "")
+title("Weekdays", line = -2)
+mtext("Interval period (T.O.D.)", side = 1, line =1.8, cex = .9)
